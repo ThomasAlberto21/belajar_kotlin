e49a5a3bd9d0a2089a5f97ae2de01029aebdbd30
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-09-14 12:00:28 +0700

    Numbers

---
+++ src/main/kotlin/numbers.kt
@@ -0,0 +1,38 @@
+fun main() {
+    // TODO : Int(32 Bit) dapat menyimpan data dari range -2^31 sampai +2^31-1. Dengan ukuran 32 Bit
+    val intNumber: Int = 100
+
+    // TODO : Long(64 Bit) dapat menyimpan data dari range -2^63 sampai +2^63-1. Dengan ukuran 64 Bit
+    val longNumber: Long = 1000000000000000000
+
+    // TODO : Short(16 Bit) dapat menyimpan data dari range -2^15 sampai +2^15-1. Dengan ukuran 16 Bit
+    val shortNumber: Short = 10
+
+    // TODO : Byte(8 Bit) dapat menyimpan data dari range -2^7 sampai +2^7-1. Dengan ukuran 8 Bit
+    val byteNumber: Byte = 127
+
+    // TODO : Double(64 Bit) dapat menyimpan data dari range 4.9E-324 sampai 1.8E+308. Dengan ukuran 64 Bit
+    val doubleNumber: Double = 1.0
+
+    // TODO : Float(32 Bit) dapat menyimpan data dari range 1.4E-45 sampai 3.4E+38. Dengan ukuran 32 Bit
+    val floatNumber: Float = 1.0F
+
+
+    // TODO : Konversi Angka
+    /*
+    toByte(): Byte
+    toShort(): Short
+    toInt(): Int
+    toLong(): Long
+    toFloat(): Float
+    toDouble(): Double
+    toChar(): Char
+    */
+
+    val stringNumber = "23"
+    val intNumbers = 3
+
+    print(intNumbers + stringNumber.toInt())
+
+}
+
