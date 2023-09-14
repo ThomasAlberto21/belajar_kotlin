1042e90ac9ad1e1b9c4ed936510eab332d58adce
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-09-14 11:02:31 +0700

    nullable types

--- src/main/kotlin/nullable_types.kt
+++ src/main/kotlin/nullable_types.kt
@@ -1,4 +1,4 @@
 fun main() {
     val text: String? = null
-    val text2: String = null // Error(Jika tidak menggunakan tanda tanya maka tidak bisa diisi null)
+//    val text2: String = null // Error(Jika tidak menggunakan tanda tanya maka tidak bisa diisi null)
 }
