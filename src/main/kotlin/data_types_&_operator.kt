2d683a3c6d699c64f42e1ed93fc6754c8f70b05e
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-09-14 11:12:27 +0700

    Data types & operator

---
+++ src/main/kotlin/data_types_&_operator.kt
@@ -0,0 +1,16 @@
+fun main(){
+    /*
+    * val : variable immutable (Variable yang tidak bisa diubah lagi setelah diinisialisasi)
+    * var : variable mutable (Variable yang bisa diubah lagi setelah diinisialisasi)
+    * */
+    val name = "Thomas alberto"
+    println("Hello my name is $name") // Menggunakan string template
+
+    // Menggabungkan dua buah string
+    val firstName = "Thomas "
+    val lastName = "Alberto "
+    println(firstName + lastName)
+
+
+}
+
