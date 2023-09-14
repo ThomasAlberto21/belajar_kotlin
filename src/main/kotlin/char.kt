a7e81b416edf477362ed05b67600c7481444311f
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-09-14 11:25:58 +0700

    Char

--- src/main/kotlin/char.kt
+++ src/main/kotlin/char.kt
@@ -1,7 +1,7 @@
 fun main() {
     // Char (Tipe data yang hanya dapat digunakan untuk menyimpan 1 karakter saja)
-    val character = 'A'
-    // (Error) val character2 = 'ABC'
+    val character: Char = 'A'
+    // (Error) val character2: Char = 'ABC'
 
     // Operasi increment dan decrement
     // TODO : Kenapa kita bisa menggunakan operasi increment dan decrement pada tipe Char? Karena pada dasarnya setiap Characters merupakan representasi dari Unicode. Contoh Unicode A adalah 0041. Ketika kita melakukan increment maka hasilnya adalah 0042 yang mana merupakan Unicode dari B.
