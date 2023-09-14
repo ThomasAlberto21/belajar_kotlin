cb3d9ee657d60a137cd10d074635f5e155d108f8
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-09-14 11:20:41 +0700

    Char

---
+++ src/main/kotlin/char.kt
@@ -0,0 +1,17 @@
+fun main() {
+    // Char (Tipe data yang hanya dapat digunakan untuk menyimpan 1 karakter saja)
+    val character = 'A'
+    val character2 = 'ABC' // Error
+
+    // Operasi increment dan decrement
+    // TODO : Kenapa kita bisa menggunakan operasi increment dan decrement pada tipe Char? Karena pada dasarnya setiap Characters merupakan representasi dari Unicode. Contoh Unicode A adalah 0041. Ketika kita melakukan increment maka hasilnya adalah 0042 yang mana merupakan Unicode dari B.
+
+    var vocal = 'A'
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+}
