fda19a5fb852207b2e95dc9d42c70052140f3265
ThomasAlberto21 <thomasalberto456@gmail.com>
2023-09-14 11:08:48 +0700

    Hello World!

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,3 @@
 fun main(){
-    /*
-    * val : variable immutable (Variable yang tidak bisa diubah lagi setelah diinisialisasi)
-    * var : variable mutable (Variable yang bisa diubah lagi setelah diinisialisasi)
-    * */
-    val name = "Thomas alberto"
-    println("Hello my name is $name")
+   println("Hello World!")
 }
